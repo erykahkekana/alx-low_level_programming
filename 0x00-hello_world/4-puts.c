c4eeb043d64f8a424d4ae6058777955c8fd4351c
erykah kekana <kekanaerykah@gmail.com>
2022-12-08 08:35:22 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,11 @@
 #include <stdio.h>
-
 /**
-
  * main - Entry
-
  *
-
  * Return: Always 0
-
  */
-
 int main(void)
-
-{	puts("\""Programming is like building a multilingual puzzle);
-
-    return (0);
-
+{
+	puts("\""Programming is like building a multiligual puzzle);
+	return (0);
+}
