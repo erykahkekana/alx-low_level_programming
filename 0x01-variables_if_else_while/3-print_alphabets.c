660f59ac2fa7b482d696b435c88d17eb96d3264e
erykah kekana <kekanaerykah@gmail.com>
2022-12-09 10:11:39 -0800

    print alphabets low and up

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet in lower case then upper case
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n');
+		return (0);
+}
+
