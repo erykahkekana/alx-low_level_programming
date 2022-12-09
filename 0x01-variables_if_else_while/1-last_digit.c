3d6b9e99776aac86f75d7e503b221def8a5afdb6
erykah kekana <kekanaerykah@gmail.com>
2022-12-09 09:46:14 -0800

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,13 +8,23 @@
  */
 int main(void)
 {
-	char ch;
+	int n;
 
-	for (ch = 'a'; ch <= 'z'; ch++)
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if ((n % 10) > 5)
 	{
-		putchar(ch);
+		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 	}
-	putchar('\n');
+	else if ((n % 10) < 6 && (n % 10) != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is 0\n", n, n % 10);
+	}
 	return (0);
 }
 
