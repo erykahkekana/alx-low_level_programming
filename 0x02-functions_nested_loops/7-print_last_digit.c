4d6ff1612bab06d163888b445913efebc353eb57
erykah kekana <kekanaerykah@gmail.com>
2022-12-13 06:20:00 -0800

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_last_digit - last digit of number
+ * @n: int to get last digit
+ * Return: value of last digit
+ */
+int print_last_digit(int n)
+{
+	int a;
+
+	if (n < 0)
+		n = -n;
+	a = n % 10;
+
+	if (a < 0)
+		a = -a;
+
+	_putchar(a + '0');
+	return (0);
+}
+
