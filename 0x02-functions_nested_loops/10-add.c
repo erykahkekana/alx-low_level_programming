b5a6a64b5bb1e93174dfd055b323bb3855ee2dd1
erykah kekana <kekanaerykah@gmail.com>
2022-12-13 06:58:23 -0800

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include  "main.h"
+
+/**
+ * add - adds two interggers
+ * @a: int added to b
+ * @b: int added to a
+ * Return: sum a and b
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
+
