20a128af6afb69bfd3182cd9553e266dec9f221a
erykah kekana <kekanaerykah@gmail.com>
2022-12-20 12:25:46 -0800

    string lentgh

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,6 +2,8 @@
 #include <string.h>
 #include "main.h"
 
+int _strlen(char *s);
+
 int main(void)
 {
 	char *s = "My first string length.";
@@ -11,3 +13,13 @@ int main(void)
 
 	return (0);
 }
+
+int _strlen(char *s)
+{
+	int len = 0;
+	while (str[len] != '\0')
+		len++;
+
+	return (len);
+}
+
