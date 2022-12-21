ab3c86cd7acc18cc14db96fea4c9f98a71cae3a2
erykah kekana <kekanaerykah@gmail.com>
2022-12-21 06:38:29 -0800

    string length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,21 +2,16 @@
 #include <string.h>
 #include "main.h"
 
-int _strlen(char *s);
-
-int main(void)
-{
-	char *s = "My first string length.";
-	int length = strlen(s);
-
-	printf("length = %d\n", length);
-
-	return (0);
-}
-
+/**
+ * _strlen - return length of string
+ * @s - string return
+ * return: Always 0 
+ */
+ 
 int _strlen(char *s)
 {
 	int len = 0;
+
 	while (str[len] != '\0')
 		len++;
 
