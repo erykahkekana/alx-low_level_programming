cbbad41142f39442071ad2a9cf7bc54de610f66d
erykah kekana <kekanaerykah@gmail.com>
2023-01-06 06:31:08 -0800

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * factorial - function returns factorial
+ * @n: number
+ * Return: factorial
+ */
+int factorial(int n)
+{
+	int n;
+	
+	if (n <= 0)
+	       return (1);
+	return (n * factorial(n - 1);
+}
+
