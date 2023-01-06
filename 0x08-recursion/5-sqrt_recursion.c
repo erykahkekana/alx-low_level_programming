f9cc46b29d3523665c047964f7ef49761b9316c0
erykah kekana <kekanaerykah@gmail.com>
2023-01-06 11:50:31 -0800

    natural square root recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,11 +5,8 @@
  * @n: natural square root
  * Return: natural square root
  */
-int _sqrt_recursion(int n);
 {
 	if (n < 0)
 		return (-1);
-	if (n == 0)
-		return (1);
-	return (_sqrt_recursion(n, 0));
+	return (actual _sqrt_recursion(n, 0));
 }
