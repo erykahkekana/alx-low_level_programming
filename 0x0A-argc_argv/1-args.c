659cdc0a7f27cccdc6502736eafb402cf29ab169
erykah kekana <kekanaerykah@gmail.com>
2023-01-11 02:21:52 -0800

    argc and argv[]

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**program prints number of arguments passed into it
+ * @argc: int
+ * @argv[]: string
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+}
+
