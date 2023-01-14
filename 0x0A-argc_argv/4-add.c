e695c6153198233f536c1defe9f51b08684fc2d8
erykah kekana <kekanaerykah@gmail.com>
2023-01-13 23:35:36 -0800

    argc and argv arguments to main

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**program adds positive numbers
+ * print result followed by new line
+ * no number passed to program print 0 followed by new line
+ * number contains symbols print error followed by new line return 1
+ * integers
+ */
+int main(int argc, char* argv[])
+{
+	int i, add = 0;
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			add += atoi(argv[i]);
+		}
+			printf("%d\n", add);
+	}
+	else
+	{
+		printf("Error\n");
+		return 1;
+	}
+	return 0;
+}
