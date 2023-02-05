6aaa8c02aea7c558a292f7755274e123524a15b5
erykah kekana <kekanaerykah@gmail.com>
2023-02-05 02:18:43 -0800

    pointers strings and arrays

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,4 +7,21 @@
  */
 void print_rev(char *s)
 {
-	int 
+	int longi = 0;
+	int o;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+	s--;
+	for (o = longi; o > 0; o--)
+	{
+		_putchar(*s);
+		s--;
+	}
+
+	_putchar('\n');
+}
+
